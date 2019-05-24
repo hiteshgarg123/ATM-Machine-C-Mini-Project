a4f981d868a6c572d5fa0059241424845b99677f
Hitesh Garg <hiteshgarg285@gmail.com>
2019-05-24 22:25:31 +0530

    This commit adds deposit function and improves the security of code

--- ATM Machine C Mini Project.cpp
+++ ATM Machine C Mini Project.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-unsigned long AccountBalance = 5000;
+unsigned long AccountBalance = 50000; 
 int temp = 0;
 
 int Withdraw();
@@ -11,33 +12,49 @@ int CarryTransaction();
 
 int MainMenu()
 {
-	int choice;
-	
-	printf("*********** Welcome to ITS ATM Machine ***********\n\n");
-	printf("1. WITHDRAW\n");
-	printf("2. DEPOSIT\n");
-	printf("3. BALANCE INQUIRY\n");
-	printf("4. MONEY TRANSFER\n\n");
-	printf("***************************************************\n\n");
-	
-	printf("\nEnter your choice\n");
-	scanf("%d", &choice);
-	
-	switch(choice)
+	int i=0;
+	while(i<3)
 	{
-		case 1: Withdraw();
-		        break;
-		case 2: Deposit();
-		        break;
-		case 3: BalanceInquiry();
-		        break;
-		case 4: MoneyTransfer();
-	            break;
-	    default: printf("Wrong Choice , Try Again\n\n");
-	             MainMenu();
+		int SecretePIN = 0 , attempt=1;
+		printf("Enter your PIN Number");
+		scanf("%d", &SecretePIN);
+		if(SecretePIN == 1992)
+		{
+			int choice; 
+		
+			printf("*********** Welcome to ITS ATM Machine ***********\n\n");
+			printf("<1>. WITHDRAW\n");
+			printf("<2>. Deposit\n");
+			printf("<3>. Balance ENQUIRY\n");
+			printf("<4>. MONEY TRANSFER\n\n");
+			printf("***************************************************\n\n");
+			
+			printf("\nEnter your choice\n");
+			scanf("%d", &choice);
+			
+			switch(choice)
+			{
+				case 1: Withdraw();
+				        break;
+				case 2: Deposit();
+				        break;
+				case 3: BalanceInquiry();
+				        break;
+				case 4: MoneyTransfer();
+			            break;
+			    default: printf("Wrong Choice , Try Again\n\n");
+			             MainMenu();
+			}
+			break;
+		}
+		else
+		{
+			i+=1;  
+			printf("Incorrect PIN, Please try again\n\n");
+		}
 	}
-	
 }
+	
 
 int CarryTransaction()
 {
@@ -46,8 +63,10 @@ int CarryTransaction()
 	printf("Press \n 1.- Yes \n 2.- No \n");
 	scanf("%d" , &temp);
 	if(temp == 1)
-	MainMenu();
-	
+	{
+		system("CLS");
+		MainMenu();
+	}
 	else if(temp == 2)
 	printf("*** THANKYOU FOR USING ATM***");
 	
@@ -72,6 +91,11 @@ int Withdraw()
 		CarryTransaction();
 	}
 	
+	else if(WithdrawalAmount>20000)
+	{
+		printf("Cannot withdraw more than 20000 at a time");
+		CarryTransaction();
+	}
 	else
 	//Eject the cash and Update the account balance
 	
@@ -84,7 +108,28 @@ int Withdraw()
 
 int Deposit()
 {
+	int DepositAmount , temp1;
+	printf("Enter the amount to be deposited");
+	scanf("%d", &DepositAmount);
 	
+	//Deposit amount cannot be greater than 20000
+	
+	if(DepositAmount>20000)
+	printf("You cannot deposit more than 20,000 at a time");
+	
+	else
+	//Update the balance and eject the recipt 
+	AccountBalance += DepositAmount;
+	printf("Balance has been updated \n Do you want the recipt ?");
+	printf("<1>. YES \n <2>. NO\n");
+	scanf("%d", &temp1);
+	if(temp1==1)
+	{
+		printf("Please Collect the Recipt");
+		CarryTransaction();
+	}
+	else
+	CarryTransaction();
 }
 
 int BalanceInquiry()
