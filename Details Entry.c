d1a26ffa0f9123af9e53d98aa470e64fa69c5e48
Hitesh Garg <hiteshgarg285@gmail.com>
2019-06-05 21:24:36 +0530

    Add files via upload

---
+++ Details Entry.c
@@ -0,0 +1,43 @@
+//This program is to enter new records 
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct employee
+{
+    char name[50] , temp[512];
+    char AccountNumber[50];
+    int pin;
+    float balance;
+} emp;
+ 
+int main()
+{
+    FILE *fp;
+    struct employee s;
+    fp = fopen("person.dat", "ab");
+ 
+    if(fp == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
+ 
+    printf("Enter Account Holder's name :\n");
+    fflush(stdin);
+    gets(s.name);
+    printf("Enter Account Number :\n");
+	fflush(stdin);
+	gets(s.AccountNumber);
+	printf("Enter PIN :\n");
+    scanf("%d",&s.pin);
+    printf("Enter Balance :\n");
+    scanf("%f",&s.balance);
+    
+    fwrite(&s,sizeof(struct employee),1,fp);
+    
+    fclose(fp);
+    
+	return 0;
+}
