52230d99e522e93a69a9afb090982fbda40c5291
Hitesh Garg <hiteshgarg285@gmail.com>
2019-06-11 11:28:05 +0530

    [Major update] - Fixed lots of errors and improved code majorly

--- ATM Machine C Mini Project.c
+++ ATM Machine C Mini Project.c
@@ -2,11 +2,13 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<string.h>  
 
 // All Global variables are declared here.
 
-int a=0,b=0,c=0,d=0,Chance=0;
+int a=0,b=0,c=0,d=0,Chance=0,tempo=0; 
+
+FILE *fp,*ftemp;
 
 struct AccountHolder
 {
@@ -15,8 +17,7 @@ struct AccountHolder
     int pin;
     float balance;
 };
-struct AccountHolder s;
-
+struct AccountHolder s,r;
 
 int MainMenu();            // Main Menu of ATM
 int Withdraw();            // Withdraw function
@@ -26,6 +27,32 @@ int MoneyTransfer();       // Money Transfer Function
 int SecurityCheck();       // This function will ask for card and PIN
 int EnhancedSecurity();    // This will take care of users who will select other options than provided ones only.
 int CardVerification();
+void update_file(struct AccountHolder);
+
+void update_file(struct AccountHolder a){
+
+	FILE *accounts, *temp;
+	struct AccountHolder b;
+
+	accounts = fopen("Account_Details.dat","rb+");
+	temp = fopen("temp.dat","wb+");
+
+	while(fread(&b, sizeof(struct AccountHolder), 1, accounts)){
+		if(strcmp(b.AccountNumber , a.AccountNumber) != 0){
+			fwrite(&b,sizeof(struct AccountHolder),1,temp);
+		}
+	}
+	fwrite(&a,sizeof(struct AccountHolder),1,temp);
+
+	fclose(accounts);
+    fclose(temp);
+	remove("Account_Details.dat");
+    int error = rename("temp.dat","Account_Details.dat");
+    if(error)
+    	printf("\n\nFile Update failed with error code %d\n", error);
+	else
+		tempo=1;
+}
 
 int MainMenu()
 {
@@ -42,7 +69,7 @@ int MainMenu()
 	printf("\t\t*                                                      *\n");
 	printf("\t\t********************************************************\n\n");
 	
-	printf("\nEnter your choice\n");
+	printf("Enter your choice\n");
 	scanf("%d", &choice);
 	
 	switch(choice)
@@ -88,20 +115,23 @@ int Withdraw()
 		}
 		else
 		{
-		//Eject the cash and Update the account balance
-	
-		FILE *fp;
-		fp=fopen("Account_Details.dat","wb+");
+			//Eject the cash and Update the account balance
 		
-		s.balance -= WithdrawalAmount;
-		fwrite(&s,sizeof(struct AccountHolder),1,fp);
-		
-		fclose(fp);
-		
-		printf("\n\n*** Please collect the cash ***\n\n");
-		printf("Current account balance is %.2f \n\n", s.balance);
-		printf("**** THANK YOU FOR USING ATM ****\n");
-		
+			s.balance -= WithdrawalAmount;
+			
+			update_file(s);
+			
+			if(tempo==1)
+			{
+				printf("\nTransaction Failed\n");
+			}
+			
+			else
+			{
+				printf("\n\n*** Please collect the cash ***\n\n");
+				printf("Current account balance is %.2f \n\n", s.balance);
+				printf("**** THANK YOU FOR USING ATM ****\n");
+			}
 		}
     }
 	else if(temp5==2)
@@ -145,32 +175,34 @@ int Deposit()
 		{
 			//Update the balance and eject the recipt 
 			 
-			FILE *fp;
-			fopen("Account_Details.dat","wb+");
-			
 			s.balance += DepositAmount;
-			fwrite(&s,sizeof(struct AccountHolder),1,fp);
+			update_file(s);
 			
-			
-			fclose(fp);
-
-			printf("Balance has been updated \nDo you want the recipt ?\n");
-			printf("\n<1>. YES \n<2>. NO\n");
-			scanf("%d", &temp1);
-			if(temp1==1)
+			if(tempo==1)
 			{
-				printf("Please Collect the Recipt\n");
+				printf("\nTransaction Failed\n");
 			}
 			
-			else if(temp1==2)
-			{
-				printf("Thank You for saving paper\n\n");
-				printf("**** THANK YOU FOR USING ATM ****\n");
-			}
 			else
 			{
-				b=1;
-				EnhancedSecurity();
+				printf("Balance has been updated \nDo you want the recipt ?\n");
+				printf("\n<1>. YES \n<2>. NO\n");
+				scanf("%d", &temp1);
+				if(temp1==1)
+				{
+					printf("Please Collect the Recipt\n");
+				}
+				
+				else if(temp1==2)
+				{
+					printf("Thank You for saving paper\n\n");
+					printf("**** THANK YOU FOR USING ATM ****\n");
+				}
+				else
+				{
+					b=1;
+					EnhancedSecurity();
+				}
 			}
 		}
 	}
@@ -200,12 +232,7 @@ int BalanceInquiry()
 	{
 		//prints the current account balance.
 		
-		FILE *fp;
-		fopen("Account_Details.dat","rb");
-		
 		printf("%.2f\n", s.balance);
-		
-		fclose(fp);
 	}
 	
 	else if(temp2==2)
@@ -224,11 +251,14 @@ int MoneyTransfer()
 {
 	//Money Transfer Function starts from here.
 	
-	int TransferAmount=0,temp3=0,temp4=0;
+	int TransferAmount=0 , temp3=0 , temp4=0;
 	char TransferAccount[50];
 	
+	printf("Enter the Account in which you want to transfer the amount\n");
+	fflush(stdin);
+	gets(TransferAccount);
 	
-	printf("Enter the amount you want to transfer\n");
+	printf("\nEnter the amount you want to transfer\n");
 	scanf("%d", &TransferAmount);
 	if(TransferAmount>40000) 
 	{
@@ -247,51 +277,58 @@ int MoneyTransfer()
 		printf("Are you sure you want to transfer %d amount", TransferAmount);
 		printf("\n<1>. YES \n<2>. NO\n");
 		scanf("%d",&temp3);
+		int flg = 0;
 		if(temp3==1)
 		{
-			
-			printf("Enter the Account in which you want to transfer the amount\n");
-			fflush(stdin);
-			gets(TransferAccount);
-			
-			FILE *fd;
-			fd=fopen("Account_Details.dat","wb+");
-			
-			if(fd == NULL)
-			printf("Failed to open file\n");
-			
-			while( fread(&s, sizeof(struct AccountHolder), 1, fd) == 1 )
+			fp = fopen("Account_Details.dat","rb+");
+			while( fread(&r, sizeof(struct AccountHolder), 1, fp))
 			{
-				if(strcmp(TransferAccount , s.AccountNumber)==0)
-				s.balance = s.balance + TransferAmount;
-				
-				else
-				printf("Invalid Account Number\n"); 
+				if(strcmp(TransferAccount , r.AccountNumber)==0)
+				{
+					flg = 1;
+					break;	
+				} 
 			}
+			fclose(fp);
 			
-			fclose(fd);  
+			if(flg == 1)
+			{
+				r.balance = r.balance + TransferAmount;
+				s.balance = s.balance - TransferAmount;
+				update_file(r);
+				update_file(s);
 
-			s.balance = s.balance - TransferAmount;
-			
-			printf("%d Amount has been Succesfully Transferred\n",TransferAmount);
-			printf("Do you want the recipt?\n");
-			printf("<1>. YES \n<2>. NO	 \n");
-			scanf("%d",&temp4);
-			if(temp4==1) 
-			{
-				printf("Please collect the recipt\n\n");
-			}
-			
-			else if(temp4==2)
-			{
-				printf("Thank you for saving paper\n");
+				if(tempo==1)
+				{
+					printf("\nTransaction Failed\n");
+				}
+				else
+				{
+					printf("\n%d Amount has been Succesfully Transferred\n",TransferAmount);
+					printf("Do you want the recipt?\n");
+					printf("<1>. YES \n<2>. NO	 \n");
+					scanf("%d",&temp4);
+					if(temp4==1) 
+					{
+						printf("Please collect the recipt\n\n");
+					}
+					
+					else if(temp4==2)
+					{
+						printf("Thank you for saving paper\n");
+					}
+					
+					else
+					{
+						printf(" *** THANKYOU FOR USING ATM ***");
+			    	}
+				}
 			}
 			
 			else
 			{
-				printf(" *** THANKYOU FOR USING ATM ***");
-		    } 
-
+				printf("\nAccount not found !\n");
+			}
 		}
 		else if(temp3==2)
 		{
@@ -335,7 +372,6 @@ int SecurityCheck()
 
 int EnhancedSecurity()
 {
-	// The function will take care of users who will press other choice than provided ones.
 	
 	Chance++;
 	while(Chance!=3)
@@ -376,66 +412,59 @@ int EnhancedSecurity()
 int CardVerification()
 {
 
-	FILE *fp;
-    char temp[50];
-    int pin=0 , flag=0 , count=0 ,abc ;
+    char AcNo[50];
+    int pin=0 , flag=0 , count=0  ;
     
-    fp = fopen("Account_Details.dat", "rb");
+    fp = fopen("Account_Details.dat", "ab+");
  
     if(fp == NULL)
     {
         exit(1);
     }
- 
-    FILE *ftemp;
-	ftemp = fopen("temp.dat","wb+");
-     
+  
 	printf("Enter your account number");
     fflush(stdin);
-    gets(temp);
+    gets(AcNo);
     
     
     while( fread(&s, sizeof(struct AccountHolder), 1, fp) == 1 )
     {
-		if(strcmp(temp , s.AccountNumber)==0)
+		if(strcmp(AcNo , s.AccountNumber)==0)
 		{
 			flag=1;
-			printf("\nWelcome %s\n",s.name);
-			
-			PINMenu:
-			printf("\nPlease enter your PIN\n");
-			scanf("%d",&pin);
-			
-			if(pin==s.pin)
-			{
-				MainMenu();  
-				fwrite(&s,sizeof(struct AccountHolder),1,ftemp);
-			}
-			
-			else
-			{
-				printf("Wrong pin , Please try again\n");
-				count++;
-				if(count!=3)
-					goto PINMenu;
-				else
-					fwrite(&s,sizeof(struct AccountHolder),1,ftemp);
-			}
+			break;
 		}
-		else
-			fwrite(&s,sizeof(struct AccountHolder),1,ftemp);
 	}
+
+	fclose(fp);
 	
 	if(flag==0)
 	{
 		printf("Please enter a valid card number\n");
 	}
-  
-    fclose(ftemp);
-	fclose(fp);
+	else{
 
-	remove("Account_Details.dat");
-    abc = rename("temp.dat","Account_Details.dat");
+		printf("\nWelcome %s\n",s.name);
+			
+		PINMenu:
+		printf("\nPlease enter your PIN\n");
+		scanf("%d",&pin);
+		
+		if(pin==s.pin)
+		{
+			MainMenu();  
+		}
+		
+		else
+		{
+			printf("Wrong pin , Please try again\n");
+			count++;
+			if(count!=3)
+				goto PINMenu;
+			else
+				printf("\nPlease try again after some time.\n");
+		}
+	}
 }
 
 int main()
